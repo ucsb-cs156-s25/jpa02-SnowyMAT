c0c59b0ab840825fc6d502d1812ef5b945dd52c3
Michael Tsai <miketsai00@gmail.com>
2025-04-03 20:22:39 -0700

    MT - added a hashCode test to TeamTest.java

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -19,6 +19,35 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+    public void equals_returns_correct_for_same_object() {
+        assertEquals(true, team.equals(team));
+    }
+
+    @Test
+    public void equals_returns_correct_for_different_class() {
+        int diff = 5;
+        assertEquals(false, team.equals(diff));
+    }
+
+    @Test
+    public void equals_returns_correct_for_casted_object() {
+        Team similar = new Team("test-team");
+        assertEquals(true, team.equals(similar));
+        similar.addMember("other");
+        assertEquals(false, team.equals(similar));
+    }
+
+    @Test
+    public void hashCode_test() {
+        int result = team.hashCode();
+        assertEquals(-1226298695, result);
+    }
    
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
